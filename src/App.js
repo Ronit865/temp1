4542d805217c9b9f9c84fe4df19e20770ab8ab1e
Masummm08 <masumdesai88@gmail.com>
2025-05-29 14:37:29 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -2,15 +2,11 @@ import { BrowserRouter as Router, Routes, Route, useLocation } from "react-route
 import Login from "./components/Login";
 import AdminDashboard from "./pages/AdminDashboard";
 import EmployeeDashboard from "./pages/EmployeeDashboard";
-import Navbar from "./components/Navbar";
-
 function AppWrapper() {
   const location = useLocation();
-  const hideNavbar = location.pathname === "/";
 
   return (
     <>
-      {!hideNavbar && <Navbar />}
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/admin" element={<AdminDashboard />} />
